3b2ceeab98f5af0901af15a4c07e269ceb71ee9a
Julia Kulla-Mader <julia.kullamader@microsoft.com>
2022-09-20 10:14:07 -0400

    Adding secret population

--- tests/github-secrets.spec.ts
+++ tests/github-secrets.spec.ts
@@ -1,5 +1,20 @@
 import { test, expect } from '@playwright/test';
 
+const sharp = require('sharp');
+var screenshotFolder = "screenshots/github-actions/";
+var screenshot1 = screenshotFolder + "github-actions-secret-menu";
+var screenshot2 = screenshotFolder + "github-actions-add-secret";
+
+var passwordName = "AZURE_CREDENTIALS";
+var passwordValue = `{
+    "appId": "generated-app-ID",
+    "displayName": "dummy-app-name",
+    "name": "http://dummy-app-name",
+    "password": "random-password",
+    "tenant": "tenant-ID"
+    }`;
+
+
 test.use({ storageState: "auth.json" });
 
 test('Secrets are visible', async ({ page }) => {
@@ -11,14 +26,34 @@ test('Secrets are visible', async ({ page }) => {
     // Check for menu item "Secrets"
     await page.locator('button:has-text("Secrets")').click();
 
-    // Open Secrets - Actions
-    await page.locator('text=Actions').nth(3).click();
+
+   // Click text=Actions >> nth=3
+   await page.locator('text=Actions').nth(3).click();
+
+   //wait for secrets menu item to be visible 
+   await expect(page.locator('[data-item-id="secrets"]')).toBeVisible()
+   await page.locator('[data-item-id="secrets"]').screenshot({ path: screenshot1 + '.png' });
+   await sharp(screenshot1 + '.png')
+            .resize({width: 240})
+            .toFile(screenshot1 + '-240px.png');
+
+
+
+    //await page.locator('text=Actions').nth(3).screenshot({ path: 'secrets-screenshot.png' });
     await expect(page).toHaveURL('https://github.com/juliakm/yaml-samples/settings/secrets/actions');
-  
-    
-    // Take a screenshot (Julia add)
-    await page.screenshot({ path: 'screenshot.png' });
-    await page.locator('data-item-id=secrets').screenshot({ path: 'secrets-screenshot.png' });
-  
+
+
+    // Click text=New repository secret
+    await page.locator('text=New repository secret').click();
+    await expect(page).toHaveURL('https://github.com/juliakm/yaml-samples/settings/secrets/actions/new');
+
+
+    // Populate secret fields
+    await page.locator('#secret_name').fill(passwordName);
+    await page.locator('#secret_value').fill(passwordValue);
+
+    // Take a screenshot of window to add secret
+    await page.locator('.container-md').screenshot({ path: screenshot2 + '.png' });
+
 });
   
